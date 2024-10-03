84553d9b909c34b2f6cb610c2c77d03df2dc04f4
Justas Dapkūnas <justas.dapkunas@gmail.com>
2024-10-03 13:42:53 +0300

    Weight inter-chain pLDDT by contact areas
    	modified:   .gitignore
    	modified:   get_interchain_plddt.py

--- get_interchain_plddt.py
+++ get_interchain_plddt.py
@@ -43,6 +43,7 @@ def run_voronota_inter_chain_contacts_script(pdb_file):
         VORONOTA_CONTACTS_SCRIPT,
         '--input', pdb_file,
         '--expand-ids',
+        '--coarse-grained',
         ]
     try:
         s = subprocess.run(
@@ -63,37 +64,50 @@ def get_interface_residues_from_contacts(voronota_js_contacts_str):
     "Get interface residue numbers from Voronota-JS contacts"
     header, contacts_data = voronota_js_contacts_str.split('\n', 1)
     fieldnames = header.split()
-    contacts = csv.DictReader(StringIO(contacts_data), delimiter='\t',
-                            fieldnames=fieldnames)
+    contacts = csv.DictReader(
+        StringIO(contacts_data), delimiter='\t', fieldnames=fieldnames
+        )
     interface_residues = {}
+    interface_residue_areas = {}
     for contact in contacts:
         chain_1 = contact['ID1_chainID']
         residue_1 = contact['ID1_resSeq']
         chain_2 = contact['ID2_chainID']
         residue_2 = contact['ID2_resSeq']
+        area = float(contact['area'])
         for c, r in ((chain_1, residue_1), (chain_2, residue_2)):
             residue_no = int(r)
+            residue_descriptor = (c, residue_no,)
             try:
                 interface_residues[c].add(residue_no)
             except KeyError:
                 interface_residues[c] = set([residue_no])
-    return interface_residues
+            try:
+                interface_residue_areas[residue_descriptor] += area
+            except KeyError:
+                interface_residue_areas[residue_descriptor] = area
+    return interface_residues, interface_residue_areas
 
 
 def all_residues_in_format_of_interface_residues(sequences_data):
     "Fake interface residues from all sequences for testing"
     fake_interface_residues = {}
+    fake_interface_areas = {}
     alphabet = string.ascii_uppercase + string.ascii_lowercase
     for description, sequence in sequences_data:
         letter, alphabet = alphabet[0], alphabet[1:]
-        fake_interface_residues[letter] = [i+1 for i in range(len(sequence))]
-    return fake_interface_residues
+        fake_interface_residues[letter] = []
+        for i in range(len(sequence)):
+            fake_interface_residues[letter].append(i+1)
+            fake_interface_areas[(letter, i+1,)] = 1
+    return fake_interface_residues, fake_interface_areas
 
 
 def get_plddt_values_for_residues(
-        model_data, sequences_data, interface_residues):
+        model_data, sequences_data, interface_residues, interface_areas):
     "Get values in pkl's pLDDT array for the interface residues"
     plddt_values = []
+    weighted_plddt_values = []
     alphabet = string.ascii_uppercase + string.ascii_lowercase
     current_sequence_start_position = 0
     for description, sequence in sequences_data:
@@ -105,12 +119,19 @@ def get_plddt_values_for_residues(
             current_sequence_start_position += len(sequence)
             continue
         for residue_no in interesting_positions:
+            residue_descriptor = (letter, residue_no,)
             plddt = model_data.plddt[
                 current_sequence_start_position + residue_no - 1
                 ]
+            weighted_plddt = plddt * interface_areas[residue_descriptor]
             plddt_values.append(plddt)
+            weighted_plddt_values.append(weighted_plddt)
+            logging.debug(
+                'Residue %s in chain %s: pLDDT %s, interface area %s',
+                residue_no, letter, plddt, interface_areas[residue_descriptor]
+                )
         current_sequence_start_position += len(sequence)
-    return plddt_values
+    return plddt_values, weighted_plddt_values
 
 
 def main(arguments):
@@ -121,9 +142,15 @@ def main(arguments):
         '--fasta', required=True, help='fasta file which was used for modeling'
         )
     arguments_parser.add_argument(
-        '--test', action='store_true', help='use_test_mode'
+        '--test', action='store_true', help='use test mode'
+        )
+    arguments_parser.add_argument(
+        '--debug', action='store_true', help='use debug logging'
         )
     args = arguments_parser.parse_args()
+
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
 
     if not voronota_js_script_present():
         print(__doc__)
@@ -134,17 +161,24 @@ def main(arguments):
     sequences_data = fasta_reader(args.fasta)
     # Processing
     if args.test:
-        interface_residues = all_residues_in_format_of_interface_residues(
-            sequences_data
-            )
+        interface_residues, interface_areas = \
+            all_residues_in_format_of_interface_residues(sequences_data)
     else:
-        interface_residues = get_interface_residues_from_contacts(contacts)
-    plddt_values = get_plddt_values_for_residues(
-        model_data, sequences_data, interface_residues
+        interface_residues, interface_areas = \
+            get_interface_residues_from_contacts(contacts)
+    plddt_values, weighted_plddt_values = get_plddt_values_for_residues(
+        model_data, sequences_data, interface_residues, interface_areas
         )
-    # Output
-    print(args.pdb_file, sum(plddt_values)/len(plddt_values),
-          model_data.global_plddt)
+    # Output:
+    #   model file
+    #   interface pLDDT
+    #   interface pLDDT weighted by areas
+    #   global pLDDT
+    print(args.pdb_file,
+          sum(plddt_values)/len(plddt_values),
+          sum(weighted_plddt_values)/sum(interface_areas.values()),
+          model_data.global_plddt
+          )
 
 
 if __name__ == '__main__':
